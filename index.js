b2effdca1318d5062fc564ed05c25278919c9eee
Patryk <Patrex97@gmail.com>
2020-03-21 20:56:58 +0100

    Map creation system

--- index.js
+++ index.js
@@ -2,49 +2,83 @@ const gameArea = document.getElementById("gameArea");
 const player = document.getElementById("player");
 const rightBorder = document.getElementById("gameArea").offsetWidth;
 const bottomBorder = document.getElementById("gameArea").offsetHeight;
-const walls = document.getElementsByClassName("wall");
-let wallCollection = new Set();
+let filePath;
 let playerX = 0;
 let playerY = 0;
+let mapInfo = [];
 
-console.log(walls[0]);
-wallCreator();
+const map1 = new Set();
+
+mapCreator()
 
 gameArea.addEventListener("keydown", playerMove);
 
-function playerMove(e){
-    setTimeout(function(){},2000);
-    switch(e.code){
-
-        case "KeyD": if(playerX + 30 < rightBorder) playerX += 30;
-        break;
-        case "KeyS": if(playerY + 30 < bottomBorder) playerY += 30;   
-        break;
-        case "KeyA": if(playerX > 0) playerX -= 30;
-        break;
-        case "KeyW": if(playerY > 0) playerY -= 30;
-        break;
-    }
-
-    player.style.left = playerX+"px";
-    player.style.top = playerY+"px";
-
+function mapCreator(){
+    fetch('maps/map1.json')
+        .then((response) => {
+            return response.json();
+        })
+        .then(data => mapInfo.push(data))
+        .then((data) => {
+            for(i = 0; i < mapInfo[0].length; i++){
+                let wall = document.createElement('div');
+                wall.setAttribute('class', 'wall');
+                wall.style.left = mapInfo[0][i].xStart+"px";
+                wall.style.top = mapInfo[0][i].yStart + "px";
+                wall.style.width = mapInfo[0][i].xEnd - mapInfo[0][i].xStart + "px";
+                wall.style.height = mapInfo[0][i].yEnd - mapInfo[0][i].yStart + "px";
+                gameArea.appendChild(wall);
+                console.log(wall);
+            }
+            
+    });
 }
 
-function wallCreator(){
-    for(let i = 0;i < 5; i++){
-        let wallX = randomInt();
-        let wallY = randomInt();
-        walls[i].style.left = wallX+"px";
-        walls[i].style.top = wallY+"px";
-        wallCollection.add({x: wallX, y: wallY});
-    }
-    
-    wallCollection.forEach(value => {
-        console.log(value.x + " " + value.y);
-    });
-} 
+function playerMove(e){
+    const walls = document.getElementsByClassName("wall");
+    let isWall = false;
+    for(let i = 0; i < walls.length; i++)
+    {
+        let wallStartX = parseInt(walls.item(i).style.left.match(/\d/g).join(""));
+        let wallStartY = parseInt(walls.item(i).style.top.match(/\d/g).join(""));
+        let wallWidth = parseInt(walls.item(i).style.width.match(/\d/g).join(""));
+        let wallHeight = parseInt(walls.item(i).style.height.match(/\d/g).join(""));
+        let wallEndX = wallWidth + wallStartX;
+        let wallEndY = wallHeight + wallStartY;
+        switch(e.code){
 
-function randomInt(){
-    return Math.floor((870 - 0) * Math.random());
-}   
+            case "KeyD": 
+            
+                if(playerX + 30  == wallStartX && (playerY >= wallStartY && playerY < wallEndY)) isWall = true;
+                break;
+
+            case "KeyS":
+                if(playerY + 30 == wallStartY && (playerX >= wallStartX && playerX < wallEndX )) isWall = true;
+                break;
+
+            case "KeyA":
+                if(playerX == wallEndX && (playerY >= wallStartY && playerY < wallEndY)) isWall = true;
+                break;
+
+            case "KeyW":
+                if(playerY == wallEndY && (playerX >= wallStartX && playerX < wallEndX )) isWall = true;
+                break;
+        } 
+        console.log(isWall);
+    }
+
+     
+    if(!isWall){
+        switch(e.code){
+            case "KeyD": if(playerX + 60 < rightBorder) playerX += 30;break;
+            case "KeyS": if(playerY + 60 < bottomBorder) playerY += 30;break;
+            case "KeyA": if(playerX > 0) playerX -= 30;break;
+            case "KeyW": if(playerY > 0) playerY -= 30;break;
+        }
+        player.style.left = playerX+"px";
+        player.style.top = playerY+"px";
+    }  
+  //  console.log("Player X: " + playerX);
+    console.log("Player Y: " + playerY);
+}
+
